dbda37792f249b5b8610e7b584ba27f4cc71076c
Your Name <you@example.com>
2025-10-02 17:08:38 -0300

    Configuracao final do Cloudinary e Strapi 5

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,18 @@
-module.exports = () => ({});
+// Arquivo: config/plugins.js
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        // Isso garante que o Strapi leia suas variáveis do EasyPanel!
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
