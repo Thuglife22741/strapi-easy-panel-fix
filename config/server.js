0e2d904482d06c6da90ff966dbaf41ce4ecd7cee
Your Name <you@example.com>
2025-10-03 04:57:59 -0300

    Fix: Remove url localhost que causou redirecionamento

--- config/server.js
+++ config/server.js
@@ -1,12 +1,9 @@
 // Arquivo: config/server.js
 
 module.exports = ({ env }) => ({
-  // ADICIONA A URL PARA GARANTIR QUE O ADMIN CONSIGA CARREGAR NO MODO DEV
-  url: env('NODE_ENV') === 'development' ? 'http://localhost:1337' : undefined,
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    // ESSA PARTE É ESSENCIAL PARA LER O APP_KEYS E JWT_SECRET
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
